2726871e544f553e6a60f461ecb97973b6025837
min.shi <minine.shi@gmail.com>
2015-03-20 16:12:21 +1100

    cache the inverse of a matrix more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## a pair of functions that cache the inverse of a matrix.
+## a pair of functions (makeCacheMatrix, cacheSolve) that cache the inverse of a matrix.
 
 ## test data:
 ## see here: http://www.mathsisfun.com/algebra/matrix-inverse.html for matrix inverse defintion.
@@ -19,7 +19,7 @@
         # [2,] -0.2  0.4
 
 makeCacheMatrix <- function(x = matrix()) {
-        ## create a special "matrix", which is a list containing a function to
+        ## create a special "matrix", which is a list containing a function to:
         # set the value of matrix,
         # get the value of the matrix
         # set the value of inverse
@@ -27,31 +27,31 @@ makeCacheMatrix <- function(x = matrix()) {
   
         i <- NULL
         
-        ## No.1, set the value of matrix
+        # No.1, set the value of matrix
         set <- function(y) {
           x <<- y
           i <<- NULL
         }
         
-        ## No.2, get the value of the matrix
+        # No.2, get the value of the matrix
         get <- function() x
         
-        ## No.3 set the value of inverse
+        # No.3 set the value of inverse of the matrix
         setinverse <- function(inverse) i <<- inverse
         
-        ## No.4, get the value of inverse
+        # No.4, get the value of inverse of the matrix
         getinverse <- function() i
         
+        # return the list
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
-## cacheSolve calculates the inverse of the special "matrix" created with the above makeCacheMatrix function
-
+## cacheSolve calculates the inverse of the special "matrix" created with the above makeCacheMatrix function.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x', 
-        ## x is an object (list) similar to the list created in above function.
+        ## x is an object (list) similar to the list created in above makeCacheMatrix function.
         
         ## checks to see if the inverse has already been calculated. 
         ## If so, it gets the inverse from the CACHE and skips the computation.
@@ -61,7 +61,7 @@ cacheSolve <- function(x, ...) {
           return(i)
         }
         
-        ## otherwise, it calculates the inverse of the data 
+        ## otherwise, it calculates the inverse of the data
         ## and sets the value of the inverse in the CACHE via the setinverse function.
         data <- x$get()
         i <- solve(data, ...)
